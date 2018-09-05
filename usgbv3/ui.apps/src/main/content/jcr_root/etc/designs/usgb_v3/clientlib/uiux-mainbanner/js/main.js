10f62701db402966199ff07dd14f9cc17f789f16
azmirrul Izzat <aaziz@apdgroup.com>
2018-09-05 11:50:54 +0800

    Disabled parallex effect and fix the carausel dotted

--- usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-mainbanner/js/main.js
+++ usgbv3/ui.apps/src/main/content/jcr_root/etc/designs/usgb_v3/clientlib/uiux-mainbanner/js/main.js
@@ -26,40 +26,40 @@
         var height = movementStrength / $(window).height();
         var width = movementStrength / $(window).width();
 
-        $(".carousel-home-banner-wrapper").mousemove(function(e){
-            var getWidth=$(window).width();
-            var newX
-            var newY 
-            if(getWidth < 768){
-                newX = 0;
-                newY = 0;
-            }
-             if(getWidth > 768){
-                newX = 50;
-                newY = 50;
-            }
-
-            if(getWidth > 1200){
-                newX = 80;
-                newY = 100;
-            }
-            if(getWidth > 1500){
-                newX = 100;
-                newY = 180;
-            }
-
-
-            var pageX = e.pageX - ($(window).width() / 2);
-            var pageY = e.pageY - ($(window).height() / 2);
-            var newvalueX = width * pageX * -1 - 25 - newX;
-            var newvalueY = height * pageY * -1 - 50 - newY;
-            $('.slick-current').css("background-position", newvalueX+"px     "+newvalueY+"px");
-        });
+//        $(".carousel-home-banner-wrapper").mousemove(function(e){
+//            var getWidth=$(window).width();
+//            var newX
+//            var newY 
+//            if(getWidth < 768){
+//                newX = 0;
+//                newY = 0;
+//            }
+//             if(getWidth > 768){
+//                newX = 50;
+//                newY = 50;
+//            }
+//
+//            if(getWidth > 1200){
+//                newX = 80;
+//                newY = 100;
+//            }
+//            if(getWidth > 1500){
+//                newX = 100;
+//                newY = 180;
+//            }
+//
+//
+//            var pageX = e.pageX - ($(window).width() / 2);
+//            var pageY = e.pageY - ($(window).height() / 2);
+//            var newvalueX = width * pageX * -1 - 25 - newX;
+//            var newvalueY = height * pageY * -1 - 50 - newY;
+//            $('.slick-current').css("background-position", newvalueX+"px     "+newvalueY+"px");
+//        });
 
 
         //remove slick dot
         var heroTileNum =  $(".hero-banner-wrapper").length
-        if(heroTileNum = 1){
+        if(heroTileNum == 1){
             $(".carousel-home-banner-wrapper .slick-dots").hide(100);
         }else{
             $(".carousel-home-banner-wrapper .slick-dots").show(100);
